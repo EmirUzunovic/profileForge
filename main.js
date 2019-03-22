e943c7d0297834e8d000dbf71ba96f4372f8c938
Emir Uzunovic <emir.uzunovic@gmail.com>
2019-03-22 22:36:56 +0100

    project cards added, paralax hero image

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 //initialize materializecss
 document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems);
+    var sidenav = document.querySelectorAll('.sidenav');
+    var sideNavInstances = M.Sidenav.init(sidenav);
+    var paralax = document.querySelectorAll('.parallax');
+    var paralaxInstances = M.Parallax.init(paralax);
   });
+
