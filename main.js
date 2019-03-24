9d59c5a4ef65a4791f37698feeb1ca4eb2771c44
Emir Uzunovic <emir.uzunovic@gmail.com>
2019-03-24 15:05:36 +0100

    footer added cards now have grid gap

--- main.js
+++ main.js
@@ -4,5 +4,6 @@ document.addEventListener('DOMContentLoaded', function() {
     var sideNavInstances = M.Sidenav.init(sidenav);
     var paralax = document.querySelectorAll('.parallax');
     var paralaxInstances = M.Parallax.init(paralax);
+    var tabs = document.querySelectorAll('.tabs')
+    var TabsInstance = M.Tabs.init(tabs);
   });
-
