3ca98e2fbafb95359b969cc5ec19694ca37938b1
Emir Uzunovic <emir.uzunovic@gmail.com>
2019-03-25 14:15:38 +0100

    projects added and hardcoded, filter by programming language is added, new style for hero image

--- main.js
+++ main.js
@@ -7,3 +7,46 @@ document.addEventListener('DOMContentLoaded', function() {
     var tabs = document.querySelectorAll('.tabs')
     var TabsInstance = M.Tabs.init(tabs);
   });
+
+
+filterSelection("all");
+
+function filterSelection(selcection){
+    let i;
+    let projects = document.querySelectorAll('.projectCard');
+    if (selcection == "all") selcection = "";
+    for (i = 0 ; i < projects.length; i++){
+        w3RemoveClass(projects[i], "show");
+        if(projects[i].className.indexOf(selcection) > -1) w3AddClass(projects[i], "show");
+    }
+}
+
+//show filtered elements
+function w3AddClass(element, name) {
+  let i;
+  let arr1;
+  let arr2;
+
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for( i = 0; i <arr2.length; i++){
+      if(arr1.indexOf(arr2[i]) == -1) {
+          element.className += " " + arr2[i];
+      }
+  }
+}
+
+//hide elements that are not selected 
+function w3RemoveClass(element, name) {
+  let i;
+  let arr1; 
+  let arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
